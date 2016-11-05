77b6c56c4096740a3b1ce816feac609527b6b0ab
cajoue <cajoue@users.noreply.github.com>
2016-11-05 21:27:11 +0100

    add about contact button

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -6,42 +6,36 @@ Template Name: About
 
 <?php get_header(); ?>
 
+<!-- get fields -->
+<?php
+	$hero_text_highlight = get_field ("hero_text_highlight");
+	$hero_text = get_field ("hero_text");
+	$intro_title = get_field('intro_title');
+?>
+
 <!-- About Hero -->
-<section class="about-page">
-	<div class="site-content">
-
-		<?php while ( have_posts() ) : the_post();
-			$hero_text_highlight = get_field ("hero_text_highlight");
-			$hero_text = get_field ("hero_text");?>
-
-			<div class="about-hero">
-				<h2>
-					<span><?php echo $hero_text_highlight; ?> </span>
-					<?php echo $hero_text; ?>
-				</h2>
-			</div>
-
-		<?php endwhile; // end of the loop. ?>
-	</div><!-- .site-content -->
+<section class="about-hero">
+	<div class="about-text">
+		<h2>
+			<span><?php echo $hero_text_highlight; ?> </span>
+			<?php echo $hero_text; ?>
+		</h2>
+	</div>
 </section><!-- .about-page -->
 
-<!-- Intro of Services -->
-<section class="about-services">
-	<div class="site-content">
-
-		<?php while ( have_posts() ) : the_post();
-			$intro_title = get_field('intro_title');?>
-
-			<h4><?php the_field('intro_title'); ?></h4>
+<!-- site content -->
+<div class="site-content">
+	<!-- Intro of Services -->
+	<section class="about-intro">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<h4><?php echo $intro_title; ?></h4>
 			<p> <?php the_content(); ?></p>
-
 		<?php endwhile; //end of the loop. ?>
-	</div><!-- .container -->
-</section><!-- .about-services -->
+	</section><!-- .about-services -->
 
-<!-- List of Services -->
-<section class="about-services-list">
-	<div class="site-content">
+	<!-- List of Services -->
+	<section class="about-services">
+
 		<?php query_posts('posts_per_page=4&post_type=services'); ?>
 
 		<?php while ( have_posts() ) : the_post();
@@ -61,8 +55,18 @@ Template Name: About
 
 		<?php endwhile; //end of the loop. ?>
 		<?php wp_reset_query(); // resets the altered query back to the original ?>
-	</div><!-- .site-content -->
-</section><!-- .about-services-list -->
+	</section><!-- .about-services -->
 
+	<!-- Contact Us -->
+	<section class="about-contact">
+
+		<div class="contact-container">
+			<h2>Interested in working with us?</h2>
+			<a class="button" href="<?php echo home_url(); ?>/contact-us">Contact Us</a>
+		</div>
+
+	</section>
+
+</div><!-- .site-content -->
 
 <?php get_footer(); ?>
